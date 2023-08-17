37939b29227917a08031501b2409f40fea9e3f72
Chanakan45 <chanakanp64@nu.ac.th>
2023-08-17 22:26:11 +0700

    page 82

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,15 @@
 let express = require('express')
+let bodyParser = require('body-parser')
+
 const app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+require('./routes')(app)
+
 app.get('/status', function (req,res){
-    res.send('Hello nodejs server belong to chanakan')
+    res.send('Hello nodejs server belong to Nattapong')
 })
 
 app.get('/hello/:name', function(req,res){
